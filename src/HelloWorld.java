0b315927f099d1f6959e58b341c610fcc88768bd
TimonSpieth <TimonSpieth@0-208-09>
2020-09-25 11:56:01 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+	static int counter = 1; 
+	
+	public static void main(String[] args) {
+
+	while (counter<=100) {
+		if(counter%3==0 && counter%5==0) {System.out.println("FizzBuzz");}
+		if (counter%3==0) {System.out.println("Fizz");}	
+	      else {
+			if(counter%5==0) {System.out.println("Buzz");}
+			else {System.out.println(counter);}
+	}
+	      counter++;
+		
+	
+	}
+	}
+}
